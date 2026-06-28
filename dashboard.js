cdbca2617f83e22291c46db023f26843016e87f6
Gamecrit2345 <gamecriticsofficial1@gmail.com>
2026-06-28 14:29:24 +0800

    Refactor balance management with localStorage

--- dashboard.js
+++ dashboard.js
@@ -1,55 +1,81 @@
-let balance = 50000;
+let balance = localStorage.getItem("balance");
 
-function updateBalance() {
+if(balance === null){
+balance = 50000;
+localStorage.setItem("balance", balance);
+}
 
-document.getElementById("balance").innerHTML =
+balance = Number(balance);
+
+function updateBalance(){
+
+document.getElementById("balance").innerText =
 "₱" + balance.toLocaleString() + ".00";
 
+localStorage.setItem("balance", balance);
+
 }
+
+/* ANIMATED UPDATE */
+
+function animateBalanceChange(oldVal, newVal){
+
+let obj = {val: oldVal};
+
+let interval = setInterval(() => {
+
+if(obj.val < newVal){
+obj.val++;
+}else if(obj.val > newVal){
+obj.val--;
+}
+
+document.getElementById("balance").innerText =
+"₱" + obj.val.toLocaleString() + ".00";
+
+if(obj.val === newVal){
+clearInterval(interval);
+}
+
+}, 10);
+
+}
+
+/* CASH IN */
 
 function cashIn(){
 
 let amount = Number(prompt("Cash In Amount"));
 
-if(amount>0){
+if(amount > 0){
+
+let old = balance;
 
 balance += amount;
 
-updateBalance();
+animateBalanceChange(old, balance);
 
-let row = `
-<tr>
-<td>Today</td>
-<td>Cash In</td>
-<td style="color:lime;">+₱${amount.toLocaleString()}</td>
-</tr>`;
-
-document.getElementById("history").innerHTML =
-row + document.getElementById("history").innerHTML;
+addHistory("Cash In", amount, "lime");
 
 }
 
 }
+
+/* SEND MONEY */
 
 function sendMoney(){
 
 let amount = Number(prompt("Send Amount"));
 
-if(amount>0 && amount<=balance){
+if(amount > 0 && amount <= balance){
 
-balance-=amount;
+let old = balance;
 
-updateBalance();
+balance -= amount;
 
-let row = `
-<tr>
-<td>Today</td>
-<td>Send Money</td>
-<td style="color:red;">-₱${amount.toLocaleString()}</td>
-</tr>`;
+animateBalanceChange(old, balance);
 
-document.getElementById("history").innerHTML =
-row + document.getElementById("history").innerHTML;
+addHistory("Send Money", -amount, "red");
 
 }else{
 
@@ -59,4 +85,36 @@ alert("Insufficient Balance");
 
 }
 
+/* HISTORY */
+
+function addHistory(type, amount, color){
+
+let row = `
+<tr>
+<td>Today</td>
+<td>${type}</td>
+<td style="color:${color};">
+${amount > 0 ? "+" : ""}₱${Math.abs(amount).toLocaleString()}
+</td>
+</tr>`;
+
+document.getElementById("history").innerHTML =
+row + document.getElementById("history").innerHTML;
+
+localStorage.setItem("history", document.getElementById("history").innerHTML);
+
+}
+
+/* LOAD HISTORY */
+
+window.onload = function(){
+
+let saved = localStorage.getItem("history");
+
+if(saved){
+document.getElementById("history").innerHTML = saved;
+}
+
 updateBalance();
+
+};
